72a85cdc7bea85dcee30d36c78bf12185235d821
userimack <mahendra.k12@gmail.com>
2025-02-16 23:24:43 +0530

    Update job post design

--- src/components/Paragraph.jsx
+++ src/components/Paragraph.jsx
@@ -1,40 +1,23 @@
-'use client';
-
-import { useState } from 'react';
-
 const Paragraph = ({
   text,
   isHtml = false,
   numberOfLines = 3,
-  expanded = false,
   className = '',
 }) => {
-  const [isExpanded, setIsExpanded] = useState(expanded);
-
-  const toggleText = () => {
-    setIsExpanded(!isExpanded);
-  };
-
   return (
-    <div className='relative text-gray-700 dark:text-gray-300 text-sm'>
+    <div
+      className={`relative text-gray-700 dark:text-gray-300 text-sm overflow-hidden ${className}`}
+      style={{
+        display: '-webkit-box',
+        WebkitBoxOrient: 'vertical',
+        WebkitLineClamp: numberOfLines,
+        textOverflow: 'ellipsis',
+      }}
+    >
       {isHtml ? (
-        <p
-          className={`${className} ${isExpanded ? '' : 'line-clamp-3'} m-0`}
-          dangerouslySetInnerHTML={{ __html: text }}
-        />
+        <p className="m-0" dangerouslySetInnerHTML={{ __html: text }} />
       ) : (
-        <p className={`${className} ${isExpanded ? '' : 'line-clamp-3'} m-0`}>
-          {text}
-        </p>
-      )}
-
-      {text.length > 100 && ( // Show the toggle only if the text is long enough
-        <button
-          onClick={toggleText}
-          className='text-blue-500 hover:text-blue-700 text-xs mt-1'
-        >
-          {isExpanded ? 'Hide' : 'Read More'}
-        </button>
+        <p className="m-0">{text}</p>
       )}
     </div>
   );
