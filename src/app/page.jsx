96df432d581fd1c7991cc3698aecb45a4a41fe8e
Rajat Rajdeep <rajatrajdeep@outlook.com>
2025-02-22 13:02:24 +0530

    Remove info text on home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,15 +2,12 @@
 import { useEffect } from 'react';
 
 import Hero from '@/components/Hero';
-import InfoAlert from '@/components/InfoAlert';
 import KeynoteSpeakers from '@/components/KeynoteSpeakers';
 import MeetOrganizers from '@/components/MeetOrganizers';
 import SponsorsSection from '@/components/Sponsors';
 import CommunityPartners from '@/components/CommunityPartners';
 
 import { SPEAKERS } from '@/speakers';
-import { Span } from '@/components/Typography';
-import Link from 'next/link';
 
 const getKeynoteSpeakers = (SPEAKERS) => {
   const speakers = new Map();
@@ -40,14 +37,6 @@ export default function Home() {
 
   return (
     <>
-      <InfoAlert>
-      <Span>
-          We are now accepting proposals for lightning talks{' '}
-        </Span>
-        <Link href="https://forms.gle/vSZcUh9CoR6PvPN79" target="_blank">
-          <Span className="underline">here.</Span>
-        </Link>  
-      </InfoAlert>
       <Hero />
       <KeynoteSpeakers speakers={getKeynoteSpeakers(SPEAKERS)} />
       <SponsorsSection />
