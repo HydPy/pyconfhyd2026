5ad56bab762f54385e8000c76a0a1efbea157fcf
Rajat Rajdeep <46029666+RajatRajdeep@users.noreply.github.com>
2025-12-18 19:50:55 +0530

    Correct typo in ABOUT_HYDPY description
    Fix typo in the description of ABOUT_HYDPY.

--- data/hydPy.js
+++ data/hydPy.js
@@ -1,7 +1,7 @@
 export const ABOUT_HYDPY = {
   title: 'Meet our Organizers',
   description:
-    'is a community-driven group that hat has been promoting Python in Hyderabad for over 9 years through regular meetups and workshops, bringing together more than 20k members. The community collaborates with other Python and open-source communities in Hyderabad and across India to broaden the adoption of Python and and open-source software (OSS). It is a recognised regional community by the global Python Software Foundation (PSF).',
+    'is a community-driven group that has been promoting Python in Hyderabad for over 9 years through regular meetups and workshops, bringing together more than 20k members. The community collaborates with other Python and open-source communities in Hyderabad and across India to broaden the adoption of Python and and open-source software (OSS). It is a recognised regional community by the global Python Software Foundation (PSF).',
   hydPyLogoUrl: '/images/hydPyLogo.svg',
   hydPyimgAlt: 'HydPy Logo',
   hydPyUrl: 'https://www.hydpy.org/',
