c0cabe16a25f54b3791fc02c5eb197e2966c1459
Joanna <joannacalamus123@gmail.com>
2026-01-23 21:53:11 +0530

    More fixes for issue #38

--- src/components/Speaker.jsx
+++ src/components/Speaker.jsx
@@ -1,28 +1,25 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import Icon from '@/components/Icon';
 import { Heading, Paragraph } from '@/components/Typography';
 
 const Speaker = ({ speaker }) => {
   return (
     <div className="flex flex-col items-center px-6 py-8 transition-transform duration-300 hover:-translate-y-2 scale-[0.98] -translate-x-[5%]">
-
-
       <Link
         className="flex flex-col items-center"
         href={`/speakers/${speaker.slug}`}
       >
         <figure className="relative w-48 lg:w-56">
-  <Image
-    src="/images/speakers/pyconfhyd26-keynote-speaker.png"
-    alt={speaker.imgAlt}
-    width={400}
-    height={600}
-    className="object-contain"
-    priority
-  />
-</figure>
+          <Image
+          src="/images/speakers/pyconfhyd26-keynote-speaker.png"
+          alt={speaker.imgAlt}
+          width={400}
+          height={600}
+          className="object-contain"
+          priority
+          />
+        </figure>
 
         <div className="relative -mt-7">
           <div className="relative z-10 bg-white border-4 border-black px-3 py-3 pb-6 text-right">
@@ -34,17 +31,13 @@ const Speaker = ({ speaker }) => {
               {speaker.name}
             </Heading>
             <Paragraph
-  level={2}
-  className="text-right font-semibold text-black"
->
-  {speaker.title}
-</Paragraph>
-
+            level={2}
+            className="text-right font-semibold text-black">
+              {speaker.title}
+              </Paragraph>
           </div>
         <div className="absolute left-0 right-0 bottom-0 top-0 translate-x-4 translate-y-5 bg-black -z-0"></div>
-
-</div>
-
+        </div>
       </Link>
     </div>
   );
