603f68c803b81b3ba2f4d4d7e0b65ef5b010b4be
Saketh-Reddy-Bejadi <bejadisakethreddy168@gmail.com>
2026-02-06 18:09:44 +0530

    feat: add community-partners

--- data/communityPartners.js
+++ data/communityPartners.js
@@ -4,4 +4,107 @@ export const COMMUNITY_PARTNERS_INFO = {
     'Applications for the Community Partner Program have closed. Our partner organizations will be featured here soon - see you at PyConf Hyderabad!',
 };
 
-export const COMMUNITY_PARTNERS = [];
+export const COMMUNITY_PARTNERS = [
+  {
+    name: 'Hyderabad DAO',
+    imgUrl: '/images/community-partners/hyd-Dao.png',
+    imgAlt: 'Hyderabad DAO logo',
+    hyperLink: 'https://linktr.ee/hyddao',
+  },
+  {
+    name: 'Cloud Native Hyderabad',
+    imgUrl: '/images/community-partners/cloud-native.png',
+    imgAlt: 'Cloud Native Hyderabad logo',
+    hyperLink: 'https://community.cncf.io/cloud-native-hyderabad/',
+  },
+  {
+    name: 'NEXTGEN NEXUS',
+    imgUrl: '/images/community-partners/Nextgen-Nexus.png',
+    imgAlt: 'NEXTGEN NEXUS logo',
+    hyperLink: 'https://bento.me/nextgen-nexus',
+  },
+  {
+    name: 'SciPy India',
+    imgUrl: '/images/community-partners/scipy-India.svg',
+    imgAlt: 'SciPy India logo',
+    hyperLink: 'https://scipy-india.github.io/',
+  },
+  {
+    name: 'PyDelhi',
+    imgUrl: '/images/community-partners/pyDelhi.png',
+    imgAlt: 'PyDelhi logo',
+    hyperLink: 'https://pydelhi.org/',
+  },
+  {
+    name: 'Django India',
+    imgUrl: '/images/community-partners/django-India.png',
+    imgAlt: 'Django India logo',
+    hyperLink: 'https://www.djangoindia.org',
+  },
+  {
+    name: 'GDG Hyderabad',
+    imgUrl: '/images/community-partners/GDG-Hyd.png',
+    imgAlt: 'GDG Hyderabad logo',
+    hyperLink: 'https://gdg.community.dev/gdg-hyderabad/',
+  },
+  {
+    name: 'Microsoft AI Innovators Hub',
+    imgUrl: '/images/community-partners/microsoft-Ai-InnovatorsHub.png',
+    imgAlt: 'Microsoft AI Innovators Hub logo',
+    hyperLink: 'https://meetup.com/microsoft-ai?member_id=428247001',
+  },
+  {
+    name: 'The Hackers Meetup',
+    imgUrl: '/images/community-partners/the-hackers-meetup.png',
+    imgAlt: 'The Hackers Meetup logo',
+    hyperLink: 'https://thehackersmeetup.org/',
+  },
+  {
+    name: 'NEXUS Swarm',
+    imgUrl: '/images/community-partners/Nexus-Swarm.png',
+    imgAlt: 'NEXUS Swarm logo',
+    hyperLink: 'https://linktr.ee/nexus_swarm',
+  },
+  {
+    name: 'Kubernetes Over Koffee',
+    imgUrl: '/images/community-partners/kubernetes-over-koffee.png',
+    imgAlt: 'Kubernetes Over Koffee logo',
+    hyperLink: 'https://www.meetup.com/kubernetes-over-koffee/',
+  },
+  {
+    name: 'Open Source Weekend',
+    imgUrl: '/images/community-partners/OSW.png',
+    imgAlt: 'Open Source Weekend logo',
+    hyperLink: 'https://osd.opensourceweekend.org/',
+  },
+  // {
+  //   name: 'Trivandrum Python Community',
+  //   imgUrl: '/images/community-partners/',
+  //   imgAlt: 'Trivandrum Python Community logo',
+  //   hyperLink: 'https://in.linkedin.com/company/trivandrum-python-community',
+  // },
+  // {
+  //   name: 'Hyderabad Data and AI Community',
+  //   imgUrl: '/images/community-partners/',
+  //   imgAlt: 'Hyderabad Data and AI Community logo',
+  //   hyperLink: 'http://aidatacommunity.com/',
+  // },
+  // {
+  //   name: 'Azure Developer Community',
+  //   imgUrl: '/images/community-partners/',
+  //   imgAlt: 'Azure Developer Community logo',
+  //   hyperLink: 'https://azdev.reskilll.com/',
+  // },
+  // {
+  //   name: 'Indian Data Club',
+  //   imgUrl: '/images/community-partners/',
+  //   imgAlt: 'Indian Data Club logo',
+  //   hyperLink: 'https://www.indiandataclub.com/',
+  // },
+  // {
+  //   name: 'Bangpypers',
+  //   imgUrl: '/images/community-partners/',
+  //   imgAlt: 'Bangpypers logo',
+  //   hyperLink: 'https://bangalore.pythonindia.org/',
+  // },
+];
