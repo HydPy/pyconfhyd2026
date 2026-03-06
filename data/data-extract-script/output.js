f9942a18bdd8f59e7a97c1e4647a7dd47c8de2c6
Abinash Panda <pandaabinash99@gmail.com>
2026-03-06 13:02:09 +0530

    Added a sample script to extract data

---
+++ data/data-extract-script/output.js
@@ -0,0 +1,52 @@
+export const OUTPUT = [
+  {
+    'Email Address': 'soumendrakumar77@gmail.com',
+    'Full name': 'Soumendra Kumar Sahoo',
+    Designation: 'AI Observability Architect at PepsiCo',
+    'Title of the Workshop/Talk:': 'Designing a Python Observability SDK',
+    'Upload a Professional Photo':
+      'https://drive.google.com/open?id=165bK6Y8DAMhE3ICRS8Ktyl6Ac6WKpynp',
+    'Bio for announcements (Max. 250 characters)':
+      'Soumendra is the AI Observability Architect at PepsiCo, where he scales global AI systems & leads sustainability initiatives for AI solutions. He joins us to share insights on building an AI Observability SDK at scale.',
+    'Website/Blog': 'https://www.soumendrak.com',
+    Linkedin: 'https://www.linkedin.com/in/soumendrak/',
+    Facebook: '',
+    X: 'https://x.com/soumendrak_',
+    'Github Repo': 'https://github.com/soumendrak/',
+    Others: 'https://links.soumendrak.com/',
+  },
+  {
+    'Email Address': 'benjaminalladi10@gmail.com',
+    'Full name': 'Benjamin Alladi',
+    Designation: 'Associate Software Engineer at OSI Digital',
+    'Title of the Workshop/Talk:':
+      'Beyond Texts: Pouring Life into Multilingual Chatbots',
+    'Upload a Professional Photo':
+      'https://drive.google.com/file/d/1ajri0N6byfi0RD-udEgpmP1JkeNwHF-F/view?usp=drive_link',
+    'Bio for announcements (Max. 250 characters)':
+      'Benjamin is a Full-Stack Developer specializing in Python and JavaScript, building scalable APIs with FastAPI, interactive apps with React and Node.js, and cloud-ready backends, focusing on clean, efficient, production-ready solutions.',
+    'Website/Blog': '',
+    Linkedin: 'www.linkedin.com/in/benjamin-alladi-383511223',
+    Facebook: '',
+    X: '',
+    'Github Repo': '',
+    Others: 'https://medium.com/@benjaminalladi10',
+  },
+  {
+    'Email Address': 'iitm.manoj@gmail.com',
+    'Full name': 'manoj ampolu',
+    Designation: 'Director, AI at Tiger Analytics ',
+    'Title of the Workshop/Talk:':
+      'Designing production grade data pipelines With Dagster ',
+    'Upload a Professional Photo':
+      'https://drive.google.com/open?id=1OHc29m-SjzyAs7-bBMfW0Rft-bu5MyUY',
+    'Bio for announcements (Max. 250 characters)':
+      'Savvy Solution Architect versed in new and advanced IT systems. Logical, organised, and decisive in building hardware and software plans. Manage all facets of projects from feasibility assessments through deployment and performance testing.',
+    'Website/Blog': 'wwwTigeranalytics.com',
+    Linkedin: 'https://in.linkedin.com/in/manoj-ampolu-454b411b',
+    Facebook: '',
+    X: '',
+    'Github Repo': '',
+    Others: '',
+  },
+];
