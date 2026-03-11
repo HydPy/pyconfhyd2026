9c5d81ccca022a71ea77dba08b3e49f39e0a3327
Abinash Panda <pandaabinash99@gmail.com>
2026-03-11 22:49:11 +0530

    Added lightning talk details

--- data/schedule.js
+++ data/schedule.js
@@ -325,10 +325,15 @@ export const SCHEDULE = {
       ],
       [
         {
-          title: 'Lightning Talks - TBA',
-          type: 'Talk',
+          title: 'Lightning Talks - Gold Sponsors',
+          type: 'Lightning Talk',
           time: '02:45 - 03:15 PM',
           location: 'Track 1 | Main Stage',
+          companies: [
+            { name: 'Epam Systems', logo: '/images/sponsors/epam.png' },
+            { name: 'Straive', logo: '/images/sponsors/straive.png' },
+            { name: 'FreJun Teler', logo: '/images/sponsors/frejun-teler.png' },
+          ],
         },
         {
           title: 'Python + Rust for High-Performance Systems',
@@ -389,10 +394,11 @@ export const SCHEDULE = {
       ],
       [
         {
-          title: 'Lightning Talks - TBA',
-          type: 'Talk',
+          title: 'Lightning Talks - General',
+          type: 'Lightning Talk',
           time: '04:15 - 04:45 PM',
           location: 'Track 1 | Main Stage',
+          companies: [],
         },
       ],
       [
